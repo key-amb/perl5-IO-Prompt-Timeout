d9a8847ff6746d0e7e05faf9f7f7cb01b4685148
key-amb <yasutake.kiyoshi@gmail.com>
2014-12-21 03:52:19 +0900

    created by minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+IO::Prompt::Timeout - It's new $module
+
+# SYNOPSIS
+
+    use IO::Prompt::Timeout;
+
+# DESCRIPTION
+
+IO::Prompt::Timeout is ...
+
+# LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+YASUTAKE Kiyoshi <yasutake.kiyoshi@gmail.com>
---
+++ lib/IO/Prompt/Timeout.pm
@@ -0,0 +1,39 @@
+package IO::Prompt::Timeout;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+IO::Prompt::Timeout - It's new $module
+
+=head1 SYNOPSIS
+
+    use IO::Prompt::Timeout;
+
+=head1 DESCRIPTION
+
+IO::Prompt::Timeout is ...
+
+=head1 LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
